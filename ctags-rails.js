6c9745f127fc98813ac06d2882d83321ccfaff50
Jordan Kicklighter <jwkicklighter@gmail.com>
2016-09-19 13:43:02 -0400

    Create ctags-rails.js

---
+++ ctags-rails.js
@@ -0,0 +1,28 @@
+var gulp  = require('gulp');
+var watch = require('gulp-watch');
+var ctags = require('gulp-ctags');
+
+gulp.task('ctags', function() {
+  var dirs = [
+    'app/**',
+    'spec/**',
+    'config/**',
+    'fixtures/**',
+    'public/**'
+  ];
+
+  return watch(dirs, function() {
+    var ctagOpts = {
+      name: 'tags',
+      concurrency: 4
+    };
+
+    gulp.src(dirs)
+      .pipe(ctags(ctagOpts))
+      .pipe(gulp.dest('./'));
+  });
+});
+
+gulp.task('default', ['ctags']);
+
+gulp.task('tags', ['ctags']);
